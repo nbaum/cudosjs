bc448fe647832d3a68dc928489b0858052ae69c1
Tugay Emin <tgntrr@gmail.com>
2022-07-20 11:52:50 +0300

    add queries

--- src/stargate/modules/group/queries.ts
+++ src/stargate/modules/group/queries.ts
@@ -2,22 +2,23 @@ import { QueryClient, createProtobufRpcClient, BankExtension } from "@cosmjs/sta
 import { assert } from "@cosmjs/utils";
 import { QueryClientImpl, QueryGroupInfoResponse, QueryGroupPolicyInfoResponse, QueryGroupMembersResponse, QueryGroupsByAdminResponse, QueryGroupPoliciesByGroupResponse, QueryGroupPoliciesByAdminResponse, QueryProposalResponse, QueryProposalsByGroupPolicyResponse, QueryVoteByProposalVoterResponse, QueryVotesByProposalResponse, QueryVotesByVoterResponse, QueryTallyResultRequest, QueryTallyResultResponse } from "./proto-types/query.pb";
 import Long from "long";
+import { PageRequest } from "./proto-types/pagination";
 
 
 export interface GroupExtension {
   readonly group: {
     readonly groupInfo: (groupId: number) => Promise<QueryGroupInfoResponse>;
-    // readonly groupPolicyInfo: (address: string) => Promise<QueryGroupPolicyInfoResponse>;
-    // readonly groupMembers: (groupId: number, paginationKey?: Uint8Array) => Promise<QueryGroupMembersResponse>;
-    // readonly groupsByMember: (address: string, paginationKey?: Uint8Array) => Promise<QueryGroupsByAdminResponse>;
-    // readonly groupPoliciesByGroup: (groupId: number, paginationKey?: Uint8Array) => Promise<QueryGroupPoliciesByGroupResponse>;
-    // readonly groupPoliciesByAdmin: (address: string, paginationKey?: Uint8Array) => Promise<QueryGroupPoliciesByAdminResponse>;
-    // readonly proposal: (proposalId: number) => Promise<QueryProposalResponse>;
-    // readonly proposalsByGroupPolicy: (address: string, paginationKey?: Uint8Array) => Promise<QueryProposalsByGroupPolicyResponse>;
-    // readonly vote: (proposalId: number, address: string) => Promise<QueryVoteByProposalVoterResponse>;
-    // readonly votesByProposal: (proposalId: number, paginationKey?: Uint8Array) => Promise<QueryVotesByProposalResponse>;
-    // readonly votesByVoter: (address: string, paginationKey?: Uint8Array) => Promise<QueryVotesByVoterResponse>;
-    // readonly tallyResult: (proposalId: number) => Promise<QueryTallyResultResponse>;
+    readonly groupPolicyInfo: (address: string) => Promise<QueryGroupPolicyInfoResponse>;
+    readonly groupMembers: (groupId: number, paginationKey?: PageRequest | undefined) => Promise<QueryGroupMembersResponse>;
+    readonly groupsByMember: (address: string, paginationKey?: PageRequest | undefined) => Promise<QueryGroupsByAdminResponse>;
+    readonly groupPoliciesByGroup: (groupId: number, paginationKey?: PageRequest | undefined) => Promise<QueryGroupPoliciesByGroupResponse>;
+    readonly groupPoliciesByAdmin: (address: string, paginationKey?: PageRequest | undefined) => Promise<QueryGroupPoliciesByAdminResponse>;
+    readonly proposal: (proposalId: number) => Promise<QueryProposalResponse>;
+    readonly proposalsByGroupPolicy: (address: string, paginationKey?: PageRequest | undefined) => Promise<QueryProposalsByGroupPolicyResponse>;
+    readonly voteByProposalVoter: (proposalId: number, voter: string) => Promise<QueryVoteByProposalVoterResponse>;
+    readonly votesByProposal: (proposalId: number, paginationKey?: PageRequest | undefined) => Promise<QueryVotesByProposalResponse>;
+    readonly votesByVoter: (voter: string, paginationKey?: PageRequest | undefined) => Promise<QueryVotesByVoterResponse>;
+    readonly tallyResult: (proposalId: number) => Promise<QueryTallyResultResponse>;
   };
 }
 
@@ -29,6 +30,39 @@ export function setupGroupExtension(base: QueryClient): GroupExtension {
     group: {
       groupInfo: async (groupId: number) => {
         return queryService.GroupInfo({ group_id: groupId });
+      },
+      groupPolicyInfo: async (address: string) => {
+        return queryService.GroupPolicyInfo({ address: address });
+      },
+      groupMembers: async (groupId: number, paginationKey?: PageRequest | undefined) => {
+        return queryService.GroupMembers({ group_id: groupId, pagination: paginationKey });
+      },
+      groupsByMember: async (address: string, paginationKey?: PageRequest | undefined) => {
+        return queryService.GroupsByMember({ address: address, pagination: paginationKey });
+      },
+      groupPoliciesByGroup: async (groupId: number, paginationKey?: PageRequest | undefined) => {
+        return queryService.GroupPoliciesByGroup({ group_id: groupId, pagination: paginationKey });
+      },
+      groupPoliciesByAdmin: async (admin: string, paginationKey?: PageRequest | undefined) => {
+        return queryService.GroupPoliciesByAdmin({ admin: admin, pagination: paginationKey });
+      },
+      proposal: async (proposalId: number) => {
+        return queryService.Proposal({ proposal_id: proposalId });
+      },
+      proposalsByGroupPolicy: async (address: string, paginationKey?: PageRequest | undefined) => {
+        return queryService.ProposalsByGroupPolicy({ address: address, pagination: paginationKey });
+      },
+      voteByProposalVoter: async (proposalId: number, voter: string) => {
+        return queryService.VoteByProposalVoter({ proposal_id: proposalId, voter: voter });
+      },
+      votesByProposal: async (proposalId: number, paginationKey?: PageRequest | undefined) => {
+        return queryService.VotesByProposal({ proposal_id: proposalId, pagination: paginationKey });
+      },
+      votesByVoter: async (voter: string, paginationKey?: PageRequest | undefined) => {
+        return queryService.VotesByVoter({ voter: voter, pagination: paginationKey });
+      },
+      tallyResult: async (proposalId: number) => {
+        return queryService.TallyResult({ proposal_id: proposalId });
       }
     },
   };
